4123a5dd5be807705d7008ee845d4db256a121c8
abdelrahman <bad606609@gmail.com>
2025-01-20 11:28:03 +0200

    setup firebase files

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_framework/responsive_wrapper.dart';
 import 'package:video_player_app/core/utils/app_router.dart';
+import 'package:video_player_app/firebase_options.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(SecureVideoPlayer());
 }
 
