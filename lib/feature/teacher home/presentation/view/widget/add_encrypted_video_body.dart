765f1ebe175fecfca904aa49c5e1974af43557f2
abdelrahman <bad606609@gmail.com>
2025-01-26 21:45:46 +0200

    ..

--- lib/feature/teacher home/presentation/view/widget/add_encrypted_video_body.dart
+++ lib/feature/teacher home/presentation/view/widget/add_encrypted_video_body.dart
@@ -181,11 +181,13 @@ class _AddEncryptedVideoBodyState extends State<AddEncryptedVideoBody> {
                   ElevatedButton(
                     onPressed: () {
                       setState(() {
-                        generatedCodesCount = int.tryParse(codeController.text) ?? 0;
+                        generatedCodesCount =
+                            int.tryParse(codeController.text) ?? 0;
                       });
                       Navigator.pop(context);
                     },
-                    style: ElevatedButton.styleFrom(backgroundColor: Colors.teal),
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.teal),
                     child: Text(
                       "Set",
                       style: TextStyle(color: Colors.white),
@@ -212,202 +214,206 @@ class _AddEncryptedVideoBodyState extends State<AddEncryptedVideoBody> {
       child: Form(
         key: formKey,
         autovalidateMode: autovalidateMode,
-        child: Column(
-          children: [
-            CustomizeTextfield(
-              text: LocaleKeys.videoLink.tr(),
-              color: kPrimaryColor,
-              onChanged: (value) {
-                videoUrl = value;
-              },
-              validator: (value) {
-                if (value?.isEmpty ?? true) {
-                  return 'Enter video URL';
-                } else {
-                  return null;
-                }
-              },
-            ),
-            const SizedBox(height: 10),
-            CustomizeTextfield(
-              text: LocaleKeys.videoTitle.tr(),
-              color: kPrimaryColor,
-              onChanged: (value) {
-                title = value;
-              },
-              validator: (value) {
-                if (value?.isEmpty ?? true) {
-                  return 'Enter video title';
-                } else {
-                  return null;
-                }
-              },
-            ),
-            const SizedBox(height: 10),
-            CustomizeTextfield(
-              text: LocaleKeys.videoDescription.tr(),
-              color: kPrimaryColor,
-              maxLines: 6,
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(LocaleKeys.grade.tr()),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.35,
-                  child: CustomDropdown(
-                      onChanged: (value) {
-                        setState(() {
-                          selectedExperienceLevel = value;
-                        });
-                      },
-                      validator: (value) {
-                        if (value?.isEmpty ?? true) {
-                          return 'Choose grade';
-                        } else {
-                          return null;
-                        }
-                      },
-                      items: [
-                        LocaleKeys.seven.tr(),
-                        LocaleKeys.eight.tr(),
-                        LocaleKeys.nine.tr(),
-                        LocaleKeys.ten.tr(),
-                        LocaleKeys.eleven.tr(),
-                        LocaleKeys.twelve.tr(),
-                      ]),
-                )
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(LocaleKeys.videoDuration.tr()),
-                GestureDetector(
-                  onTap: () {
-                    showDurationPicker(context);
-                  },
-                  child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 12, vertical: 7),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        color: kPrimaryColor),
-                    child: Text(
-                      videoDuration,
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                )
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Generated Codes Count"),
-                GestureDetector(
-                  onTap: () {
-                    showGeneratedCodesBottomSheet(context);
-                  },
-                  child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 12, vertical: 7),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        color: kPrimaryColor),
-                    child: Text(
-                      "$generatedCodesCount",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                )
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Will the video be available for platform?"),
-                Switch(
-                  value: isVideoAvailableForPlatform,
-                  onChanged: (bool value) {
-                    setState(() {
-                      isVideoAvailableForPlatform = value;
-                    });
-                  },
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(LocaleKeys.visibility.tr()),
-                Switch(
-                  value: isVideoVisible,
-                  onChanged: (bool value) {
-                    setState(() {
-                      isVideoVisible = value;
-                    });
-                  },
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(LocaleKeys.canBeExpired.tr()),
-                Switch(
-                  value: isVideoExpirable,
-                  onChanged: (bool value) {
-                    setState(() {
-                      isVideoExpirable = value;
-                    });
-                    if (value) {
-                      showExpiryDatePicker(context);
-                    }
-                  },
-                ),
-              ],
-            ),
-            if (isVideoExpirable && expiryDate != null)
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(LocaleKeys.expiryDate.tr()),
-                    Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              CustomizeTextfield(
+                text: LocaleKeys.videoLink.tr(),
+                color: kPrimaryColor,
+                onChanged: (value) {
+                  videoUrl = value;
+                },
+                validator: (value) {
+                  if (value?.isEmpty ?? true) {
+                    return 'Enter video URL';
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              const SizedBox(height: 10),
+              CustomizeTextfield(
+                text: LocaleKeys.videoTitle.tr(),
+                color: kPrimaryColor,
+                onChanged: (value) {
+                  title = value;
+                },
+                validator: (value) {
+                  if (value?.isEmpty ?? true) {
+                    return 'Enter video title';
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              const SizedBox(height: 10),
+              CustomizeTextfield(
+                text: LocaleKeys.videoDescription.tr(),
+                color: kPrimaryColor,
+                maxLines: 6,
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(LocaleKeys.grade.tr()),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.35,
+                    child: CustomDropdown(
+                        onChanged: (value) {
+                          setState(() {
+                            selectedExperienceLevel = value;
+                          });
+                        },
+                        validator: (value) {
+                          if (value?.isEmpty ?? true) {
+                            return 'Choose grade';
+                          } else {
+                            return null;
+                          }
+                        },
+                        items: [
+                          LocaleKeys.seven.tr(),
+                          LocaleKeys.eight.tr(),
+                          LocaleKeys.nine.tr(),
+                          LocaleKeys.ten.tr(),
+                          LocaleKeys.eleven.tr(),
+                          LocaleKeys.twelve.tr(),
+                        ]),
+                  )
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(LocaleKeys.videoDuration.tr()),
+                  GestureDetector(
+                    onTap: () {
+                      showDurationPicker(context);
+                    },
+                    child: Container(
                       padding:
-                          EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                          EdgeInsets.symmetric(horizontal: 12, vertical: 7),
                       decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(8),
                           color: kPrimaryColor),
                       child: Text(
-                        "${expiryDate?.toLocal().toString().split(' ')[0]} ${expiryDate?.hour}:${expiryDate?.minute}",
-                        style: TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.bold),
+                        videoDuration,
+                        style: TextStyle(color: Colors.white),
                       ),
                     ),
-                  ],
-                ),
+                  )
+                ],
               ),
-            const SizedBox(height: 15),
-            CustomButton(
-              title: LocaleKeys.add.tr(),
-              color: Colors.deepPurple,
-              onTap: () {
-                if (formKey.currentState!.validate()) {
-                  formKey.currentState!.save();
-                } else {
-                  autovalidateMode = AutovalidateMode.always;
-                  setState(() {});
-                }
-              },
-            )
-          ],
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Generated Codes Count"),
+                  GestureDetector(
+                    onTap: () {
+                      showGeneratedCodesBottomSheet(context);
+                    },
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 12, vertical: 7),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: kPrimaryColor),
+                      child: Text(
+                        "$generatedCodesCount",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Will the video be available for platform?"),
+                  Switch(
+                    value: isVideoAvailableForPlatform,
+                    onChanged: (bool value) {
+                      setState(() {
+                        isVideoAvailableForPlatform = value;
+                      });
+                    },
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(LocaleKeys.visibility.tr()),
+                  Switch(
+                    value: isVideoVisible,
+                    onChanged: (bool value) {
+                      setState(() {
+                        isVideoVisible = value;
+                      });
+                    },
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(LocaleKeys.canBeExpired.tr()),
+                  Switch(
+                    value: isVideoExpirable,
+                    onChanged: (bool value) {
+                      setState(() {
+                        isVideoExpirable = value;
+                      });
+                      if (value) {
+                        showExpiryDatePicker(context);
+                      }
+                    },
+                  ),
+                ],
+              ),
+              if (isVideoExpirable && expiryDate != null)
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(LocaleKeys.expiryDate.tr()),
+                      Container(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: kPrimaryColor),
+                        child: Text(
+                          "${expiryDate?.toLocal().toString().split(' ')[0]} ${expiryDate?.hour}:${expiryDate?.minute}",
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              const SizedBox(height: 15),
+              CustomButton(
+                title: LocaleKeys.add.tr(),
+                color: Colors.deepPurple,
+                onTap: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                  } else {
+                    autovalidateMode = AutovalidateMode.always;
+                    setState(() {});
+                  }
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
